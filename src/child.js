3a97a3d6e2b1a9604619cbcb35b2fff50a5a70bb
koolii <kuriyama@createentrance.com>
2018-01-03 07:38:48 +0100

    :sparkles: implement async(promise) without eslint-errors

--- src/child.js
+++ src/child.js
@@ -1,15 +1,34 @@
-process.on('uncaughtException', (err) => {
-  console.log(`uncaughtException: ${err}`)
-})
+process.on('uncaughtException', err => {
+  process.send(Promise.reject(err));
+});
 
-process.on('unhandledException', (err) => {
-  console.log(`unhandledException: ${err}`)
-})
+process.on('unhandledException', err => {
+  process.send(Promise.reject(err));
+});
 
-process.on('message', (arg) => {
-  const { expression, argument } = JSON.parse(arg, (k, v) => (
-    k === 'expression' ? Function.call(this, `return ${v}`)() : v
-  ))
-  const result = expression(...argument)
-  process.send({ result })
-})
+// VMモジュールを使ってglobal変数を動的に生成できないか？ただし、
+// ここはchild_processということは考えなければならない
+// const dependencies = [];
+
+process.on('message', arg => {
+  const { expression, argument } = JSON.parse(arg, (k, v) => {
+    if (k === 'dependencies') {
+      Object.values(v).forEach(d => {
+        // dependencies[d] = require(d);
+        global[d] = require(d);
+      });
+      return;
+    }
+
+    /* eslint-disable no-useless-call */
+    return k === 'expression' ? Function.call(null, `return ${v}`)() : v;
+  });
+
+  Promise.resolve()
+    .then(() => {
+      return expression(...argument);
+    })
+    .then(result => {
+      process.send({ result });
+    });
+});
