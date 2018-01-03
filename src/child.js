cf112fc2c9e7f183285bd13b073dfd8efbb7716a
koolii <kuriyama@createentrance.com>
2018-01-03 07:45:13 +0100

    :shower: resolve eslint errors

--- src/child.js
+++ src/child.js
@@ -1,34 +1,31 @@
-process.on('uncaughtException', err => {
-  process.send(Promise.reject(err));
-});
+process.on('uncaughtException', (err) => {
+  process.send(Promise.reject(err))
+})
 
-process.on('unhandledException', err => {
-  process.send(Promise.reject(err));
-});
+process.on('unhandledException', (err) => {
+  process.send(Promise.reject(err))
+})
 
 // VMモジュールを使ってglobal変数を動的に生成できないか？ただし、
 // ここはchild_processということは考えなければならない
-// const dependencies = [];
+// const dependencies = []
 
-process.on('message', arg => {
+process.on('message', (arg) => {
   const { expression, argument } = JSON.parse(arg, (k, v) => {
     if (k === 'dependencies') {
-      Object.values(v).forEach(d => {
-        // dependencies[d] = require(d);
-        global[d] = require(d);
-      });
-      return;
+      Object.values(v).forEach((d) => {
+        // dependencies[d] = require(d)
+        global[d] = require(d)
+      })
     }
 
     /* eslint-disable no-useless-call */
-    return k === 'expression' ? Function.call(null, `return ${v}`)() : v;
-  });
+    return k === 'expression' ? Function.call(null, `return ${v}`)() : v
+  })
 
   Promise.resolve()
-    .then(() => {
-      return expression(...argument);
+    .then(() => expression(...argument))
+    .then((result) => {
+      process.send({ result })
     })
-    .then(result => {
-      process.send({ result });
-    });
-});
+})
--- src/fork.js
+++ src/fork.js
@@ -1,51 +1,51 @@
-const path = require('path');
-const childProcess = require('child_process');
+const path = require('path')
+const childProcess = require('child_process')
 
-const replacer = (k, v) => (typeof v === 'function' ? v.toString() : v);
-const fork = argument => {
-  return new Promise((resolve, reject) => {
+const replacer = (k, v) => (typeof v === 'function' ? v.toString() : v)
+const fork = argument => (
+  new Promise((resolve, reject) => {
     try {
       const child = childProcess.fork('./child_process', [], {
         cwd: path.resolve(__dirname),
         // stdio: 'inherit',
         // silent: true,
-        env: {}
-      });
+        env: {},
+      })
 
       if (!child.connected) {
-        reject(new Error('Not Connected to child process'));
+        reject(new Error('Not Connected to child process'))
       }
 
-      child.on('message', result => {
+      child.on('message', (result) => {
         if (child.connected) {
-          child.disconnect();
+          child.disconnect()
         }
 
         if (Object.keys(result.result) !== 0) {
-          resolve(result);
+          resolve(result)
         }
-      });
+      })
 
       child.on('disconnect', () => {
-        process.kill(child.pid);
-      });
+        process.kill(child.pid)
+      })
 
-      console.log(`[parent] process.pid: ${process.pid}`);
+      console.log(`[parent] process.pid: ${process.pid}`)
 
-      child.send(JSON.stringify(argument, replacer));
+      child.send(JSON.stringify(argument, replacer))
     } catch (err) {
-      reject(new Error(`too much requests: ${argument}`));
+      reject(new Error(`too much requests: ${argument}`))
     }
-  });
-};
+  })
+)
 
-process.on('unhandledRejection', err => {
-  // console.log(`unhandledRejection: ${JSON.stringify(err)}`);
-  console.log(`unhandledRejection: ${err}`);
-});
-process.on('uncaughtException', err => {
-  // console.log(`uncaughtException: ${JSON.stringify(err)}`);
-  console.log(`uncaughtException: ${err}`);
-});
+process.on('unhandledRejection', (err) => {
+  // console.log(`unhandledRejection: ${JSON.stringify(err)}`)
+  console.log(`unhandledRejection: ${err}`)
+})
+process.on('uncaughtException', (err) => {
+  // console.log(`uncaughtException: ${JSON.stringify(err)}`)
+  console.log(`uncaughtException: ${err}`)
+})
 
-module.exports = fork;
+module.exports = fork
